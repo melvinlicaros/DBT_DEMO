eb215f1ed86269db1dcd7135018546920092812d
Melvin Licaros <melvinlicaros@gmail.com>
2025-11-25 11:14:54 +0000

    station_demension model created

---
+++ models/station_dimension.sql
@@ -0,0 +1,14 @@
+WITH BIKE AS (
+
+select 
+    distinct
+    start_statio_id AS station_id,
+    start_station_name,
+    start_lat,
+    start_lng
+from {{ source('demo', 'bike') }}
+WHERE RIDE_ID != 'ride_id'
+)
+
+select *
+from BIKE
